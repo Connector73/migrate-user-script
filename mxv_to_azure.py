5587ae2c5ca9abfdc4cb1835f2f70056165718aa
mmadraimov <42767360+mmadraimov@users.noreply.github.com>
2018-09-25 15:16:23 +0300

    Update mxv_to_azure.py

--- mxv_to_azure.py
+++ mxv_to_azure.py
@@ -2,10 +2,10 @@ import csv
 import json
 import string
 import secrets
+import argparse
 
 from subprocess import check_output, call
 
-mxv_csv = "/path/to/csv/demo.csv"
 
 fieldnames = ("TenantID","First Name","Last Name","Extension","Voice DID","Fax DID","Caller ID","ID for MS Exchange","Home Phone","Cell Phone","Fax Number",
                 "E-mail","Alternate E-mail","User Name","Password","PIN","Pseudonym","User Profile","ID","Admin Profile","Paging Profile","Recording Profile","Home MX",
@@ -17,19 +17,29 @@ def azure_password():
     password += ''.join(secrets.choice(string.digits) for _ in range(4))
     return password
 
-# Login
-call(['az', 'login', '--allow-no-subscription'])
+  
+def main(mxv_csv):
+    # Login
+    call(['az', 'login', '--allow-no-subscription'])
 
-with open(mxv_csv, 'r') as csvfile:
-    reader = csv.DictReader(csvfile,fieldnames)
-    next(reader)
+    with open(mxv_csv, 'r') as csvfile:
+        reader = csv.DictReader(csvfile,fieldnames)
+        next(reader)
 
-    for row in reader:
-        try:
-            result = check_output(['az', 'ad', 'user', 'create', '--display-name', ' '.join([row['First Name'],row['Last Name']]),
-                            '--password', azure_password(),'--force-change-password-next-login', 'true', '--user-principal-name', row['E-mail']]).decode('utf-8')
-            print('Created:', json.loads(result)['userPrincipalName'])
-        except Exception as e:
-            print(e)
+        for row in reader:
+            try:
+                result = check_output(['az', 'ad', 'user', 'create', '--display-name', ' '.join([row['First Name'],row['Last Name']]),
+                                '--password', azure_password(),'--force-change-password-next-login', 'true', '--user-principal-name', row['E-mail']]).decode('utf-8')
+                print('Created:', json.loads(result)['userPrincipalName'])
+            except Exception as e:
+                print(e)
 
-exit(0)
+    exit(0)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    requiredArg = parser.add_argument_group('required arguments')
+    requiredArg.add_argument('--mxv-file', dest='mxvfile', help='MXvirtual exported user list file', metavar='FILE', required=True)
+    
+    args = parser.parse_args()
+    main(args.mxvfile)
